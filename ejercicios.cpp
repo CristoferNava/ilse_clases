c9c17fea530414e3bfa5772aed38b885f276164f
CristoferNava <cj.carmonanava@ugto.mx>
2021-01-05 18:29:15 -0600

    ejercecios/05/enero/21

--- ejercicios.cpp
+++ ejercicios.cpp
@@ -1,1 +1,11 @@
-// 1 2 3 4 6 7 8 9 10
+// Pedir al usuario un número no entero no negativo y sumar todos los enteros
+// consecutivos a partir del 1 hasta el número el usuario
+// n = 100
+// 1 + 2 + 3 + 4 + ... + 100 = 5050
+
+// Imprimir los pares entre 1 y n
+// n = 10 => 2, 4, 6, 8, 10
+
+// Investigar para qué sirve break y continue
+// secret = 35
+// estoy pensando un número entre el 1 y 70
